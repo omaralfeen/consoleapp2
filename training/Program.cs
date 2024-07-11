d7ce9556bf6cd651faac32d869a0567de0937c7e
FORMAT <FORMAT@DESKTOP-E2BEAN4>
2024-07-11 13:29:01 +0300

    Add project files.

---
+++ training/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace omar
+{
+    class student
+    {
+        int age;
+        int ssn;
+        string name;
+
+        public void personal(int Age , string Name,int SSN) {
+            age = Age;
+            ssn = SSN;
+            name = Name;
+        }
+        public void detalies()
+        {
+            Console.WriteLine($"{age} - {name} - {ssn}");
+        }
+    }
+    class program
+    {
+        static void Main(string[] arge)
+        {
+            //1
+            Console.WriteLine(" enter  a number");
+            int x=int.Parse(Console.ReadLine());
+            int i;
+            int y;
+            for (i = 0;i<=12; i++) {
+                y = x * i;
+                Console.WriteLine($"{x} * {i}={y}");
+            }
+            //2
+            Console.WriteLine("enter your age");
+            int age = -1;
+            do
+            {
+                age = int.Parse(Console.ReadLine());
+            } while (age<0);
+            //3
+
+            student st = new student();
+            st.personal( 20,"omar",995588);
+            st.detalies();
+        }
+    }
+}
+
+
+        
